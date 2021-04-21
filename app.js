0bf8dc194585f5a1384e8fa3a7cf30f25da77b0b
ljwedin <ljwedin@gmail.com>
2021-04-21 14:28:41 +0200

    Creates error html page for login, moves admin page to separate folder and connects stylesheets

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin', express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
-/* GET users listing. */
+const loginError = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Logga in</title>
+        <link rel="stylesheet" href="./stylesheets/style.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main">
+            <h1>Logga in som admin</h1>
+            <form method="POST">
+                <input type="password" name="password" id="password" />
+                <input type="submit" id="loginBtn" value="Logga in" />
+                <p>Fel lösenord! Prova igen.</p>
+            </form>
+        </div>
+    </body>
+    </html>`;
+
 router.get('/', (req, res, next) => {
-//   req.app.locals.db.collection('users').find().toArray()
-//   .then(results => {
-//     console.log(results);
-//   })
-  
-//   res.send('respond with a resource');
+    res.sendFile('login.html', { root: './public'});
 });
 
-router.post('/add', (req, res) => {
-//   req.app.locals.db.collection('users').insertOne(req.body)
-//   .then(result => {
-//     console.log(result);
-//     res.redirect('/');
-//   })
+router.post('/', (req, res) => {
+    if (req.body.password === 'admin') {
+        res.sendFile('admin.html', {root: './admin'});
+    } else {
+        res.send(loginError);
+    }
 })
 
 module.exports = router;
