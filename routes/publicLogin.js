c6d51b0e37948d26f15c25906af4ebfa3879eed0
ljwedin <ljwedin@gmail.com>
2021-04-22 21:10:04 +0200

    Spent one hour trying to figure out why postman didn't work and realised that I wrote use(cors) instead of use(cors())

--- routes/publicLogin.js
+++ routes/publicLogin.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile('./public/userPage.html');
+    let user = req.body;
+    console.log(user);
+    let testRes = true;
+    res.send(testRes);
 });
 
-router.post('/add', (req, res) => {
-  req.app.locals.db.collection('users').insertOne(req.body)
-  .then(result => {
-    console.log(result);
-    res.redirect('/');
-  })
-})
+router.post('/', (req, res) => {
+    res.send('Hallå från servern!');
+});
 
 module.exports = router;
