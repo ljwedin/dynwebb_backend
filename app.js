f24ae904c140674ea054a412bedf71bbc150f319
ljwedin <ljwedin@gmail.com>
2021-04-23 18:04:18 +0200

    Creates test GET function from logged in user to retrieve user data

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin')
 const publicRouter = require('./routes/publicLogin');
+const userRender = require('./routes/userRender');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/login', publicRouter);
+app.use('/userpage', userRender);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/userRender.js
+++ routes/userRender.js
@@ -2,36 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    let user = req.body;
-    console.log(user);
-    let testRes = true;
-    res.send('testRes');
+    req.app.locals.db.collection('users').find().toArray()
+    .then(results => {
+        console.log(req.body);
+        let user = results.find(req.body._id);
+        res.send(user);
+    })
 });
 
-// POST test
-// router.post('/', (req, res) => {
-//     let testRes = true;
-//     res.send(testRes);
-// });
 
 router.post('/', (req, res) => {
     req.app.locals.db.collection('users').find().toArray()
     .then(results => {
-        let id;
-
         for (let user in results) {
             if (req.body.userName === results[user].userName) {
                 if (req.body.password === results[user].password) {
                     const userObject = results[user];
-                    id = userObject._id;
+                    const id = userObject._id;
+                    res.send(id);
+                    break;
                 }
             }
         }
-
-        if (id === undefined) {
-            id = false;
-        }
-        res.send(id);
     });
 });
 
