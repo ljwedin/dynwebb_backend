36963ae491566ffccbb03d3f3c515ad9ba4d71cc
ljwedin <ljwedin@gmail.com>
2021-04-23 21:35:02 +0200

    Creates endpoint function for userpage to send user info

--- routes/userRender.js
+++ routes/userRender.js
@@ -11,20 +11,51 @@ router.get('/', (req, res, next) => {
 });
 
 
+// router.post('/', (req, res) => {
+//     req.app.locals.db.collection('users').find().toArray()
+//     .then(results => {
+//         for (let user in results) {
+//             if (req.body.userName === results[user].userName) {
+//                 if (req.body.password === results[user].password) {
+//                     const userObject = results[user];
+//                     const id = userObject._id;
+//                     res.send(id);
+//                     break;
+//                 }
+//             }
+//         }
+//     });
+// });
+
 router.post('/', (req, res) => {
     req.app.locals.db.collection('users').find().toArray()
     .then(results => {
+        let userInfo;
+
+        // console.log(typeof req.body.id);
+        // console.log(typeof JSON.parse(results[3]._id));
+
         for (let user in results) {
-            if (req.body.userName === results[user].userName) {
-                if (req.body.password === results[user].password) {
-                    const userObject = results[user];
-                    const id = userObject._id;
-                    res.send(id);
-                    break;
+            // let dbUser = JSON.parse(results[user]);
+            console.log(results[user]);
+            if (req.body.id == results[user]._id) {
+                const userObject = results[user];
+                userInfo = {
+                    userName: userObject.userName,
+                    email: userObject.email,
+                    newsletter: userObject.newsletter
                 }
             }
+
         }
+
+        if (userInfo == undefined) {
+            userInfo = false;
+        }
+
+        res.send(userInfo);
     });
 });
+
 
 module.exports = router;
