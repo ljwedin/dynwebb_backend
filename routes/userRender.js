4a7d317dbb62ec76ee10756794a652eb235b1c4b
ljwedin <ljwedin@gmail.com>
2021-04-24 15:31:29 +0200

    Adds functions for updating email and newsletter

--- routes/userRender.js
+++ routes/userRender.js
@@ -40,7 +40,31 @@ router.post('/updatename', (req, res) => {
     const newUserName = req.body.userName;
     const id = req.body.id;
 
-    req.app.locals.db.collection('users').updateOne({ _id: ObjectID(id) }, {$set: {userName: newUserName}})
+    req.app.locals.db.collection('users').updateOne({ _id: ObjectID(id) }, { $set: { userName: newUserName }})
+    .then(result => {
+        const updateResultObject = JSON.parse(result);
+        const updateResult = { result: updateResultObject.result.n };
+        res.send(updateResult);
+    })  
+})
+
+router.post('/updateemail', (req, res) => {
+    const newEmail = req.body.email;
+    const id = req.body.id;
+
+    req.app.locals.db.collection('users').updateOne({ _id: ObjectID(id) }, {$set: { email: newEmail }})
+    .then(result => {
+        const updateResultObject = JSON.parse(result);
+        const updateResult = { result: updateResultObject.result.n };
+        res.send(updateResult);
+    })  
+})
+
+router.post('/updatenewsletter', (req, res) => {
+    const newNewsletter = req.body.newsletter;
+    const id = req.body.id;
+
+    req.app.locals.db.collection('users').updateOne({ _id: ObjectID(id) }, { $set: { newsletter: newNewsletter }})
     .then(result => {
         const updateResultObject = JSON.parse(result);
         const updateResult = { result: updateResultObject.result.n };
