8b327b9a0bb9e6622552b74e97efc6492a3571d9
ljwedin <ljwedin@gmail.com>
2021-04-25 12:21:22 +0200

    Changed the landing page of server to be admin login page

--- app.js
+++ app.js
@@ -38,9 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 
-app.use('/', indexRouter);
+app.use('/', adminRouter);
 app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
 app.use('/login', publicRouter);
 app.use('/userpage', userRender);
 app.use('/newuser', newUser);
